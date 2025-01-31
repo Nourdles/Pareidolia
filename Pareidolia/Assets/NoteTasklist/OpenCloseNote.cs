6abae2cae27d3adbc9b56392ac57f8d7c6579f02
jacqueline zhu <jacquelinezhu999@gmail.com>
2025-01-30 21:28:41 -0500

    created note object and UI + added TMP

---
+++ Pareidolia/Assets/NoteTasklist/OpenCloseNote.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using TMPro;
+
+/// <summary>
+/// Script for opening and closing notes
+/// </summary>
+public class OpenCloseNote : MonoBehaviour
+{   
+    public KeyCode closeKey; // key to press to exit note
+    public Rigidbody player;
+
+    public GameObject noteCanvas;
+    public TMP_Text noteTextAreaUI;
+    public string text; // text to appear on the note
+
+    private bool noteOpen = false;
+
+    public void OpenNote()
+    {
+        noteTextAreaUI.text = text;
+        noteCanvas.SetActive(true);
+
+        // stop player from moving while reading
+        DisablePlayer();
+
+        noteOpen = true;
+    }
+
+    public void CloseNote()
+    {
+        noteCanvas.SetActive(false);
+
+        // enable player movement
+        EnablePlayer();
+
+        noteOpen = false;
+    }
+
+    public void DisablePlayer()
+    {
+        player.constraints = RigidbodyConstraints.FreezePosition;
+    }
+
+    public void EnablePlayer()
+    {
+        player.constraints = RigidbodyConstraints.None;
+    }
+
+    private void Update()
+    {
+        if (noteOpen)
+        {
+            if (Input.GetKeyDown(closeKey))
+            {
+                CloseNote();
+            }
+        }
+    }
+
+    
+}
