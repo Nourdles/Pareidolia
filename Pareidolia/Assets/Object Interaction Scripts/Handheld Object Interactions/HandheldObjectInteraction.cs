d3976706db99563831e19e305324419a8aa12952
villanueva-j <113061736+villanueva-j@users.noreply.github.com>
2025-02-19 13:17:25 -0500

    Added invisible player hands

--- Pareidolia/Assets/Object Interaction Scripts/Handheld Object Interactions/HandheldObjectInteraction.cs
+++ Pareidolia/Assets/Object Interaction Scripts/Handheld Object Interactions/HandheldObjectInteraction.cs
@@ -15,6 +15,19 @@ public abstract class HandheldObjectInteraction : ObjectInteraction
         } else
         {
             PickUpEvent?.Invoke(gameObject);
+            PickUp();
         }
     }
+
+    // make a fxn here to move the object
+    private void PickUp()
+    {
+        // parent the object to the camera
+        // gameObject.position = 
+        // remove rigidbody
+        // do not detect collisions
+        // rigid body isKinematic to true
+    }
+
+    //protected abstract void MoveToCamView(); // to move the object to a set position
 }
