4541c4779989689dee529f43aff639bd9cafdf85
villanueva-j <113061736+villanueva-j@users.noreply.github.com>
2025-02-25 16:27:29 -0500

    Created bathroom interactable scripts

---
+++ Pareidolia/Assets/Object Interaction Scripts/Bathroom Interactables/ShowerCurtainInteraction.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ShowerCurtainInteraction : ObjectInteraction
+{
+    public override void interact(GameObject objectInHand)
+    {
+        throw new System.NotImplementedException();
+        // if it's closed, open
+        // if it's open, close it
+        // can stay as interactable
+    }
+
+}
