91386688a67d6d9f27751ed651149058cac9111d
villanueva-j <113061736+villanueva-j@users.noreply.github.com>
2025-02-25 15:15:35 -0500

    Create TasksEnum.cs
    Created enum for tasks

---
+++ Pareidolia/Assets/Task Scripts/TasksEnum.cs
@@ -0,0 +1,15 @@
+public enum Tasks
+{
+    // Morning Tasks
+    MakeBed,
+    MakeBreakfast,
+    //WashLaundry,
+    Shower,
+    // Afternon Tasks
+    PickupTrash,
+    DryLaundry,
+    CookRamen,
+    WashDishes,
+    WatchTV,
+    // Night Tasks
+}
