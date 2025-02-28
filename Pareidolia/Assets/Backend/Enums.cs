d8eefd4be2ee8dab610c70b08e9b18135f474529
jacqueline zhu <jacquelinezhu999@gmail.com>
2025-02-27 21:41:46 -0500

    renamed LevelEnum file to Enums, cleaned up spacing inside code

---
+++ Pareidolia/Assets/Backend/Enums.cs
@@ -0,0 +1,10 @@
+
+public enum Levels
+{
+    Tutorial = 0,
+    Morning = 1,
+    Afternoon = 2,
+    Evening = 3
+}
+
+
--- Pareidolia/Assets/Backend/LevelsEnum.cs
+++
@@ -1,9 +0,0 @@
-
-
-    public enum Levels
-    {
-        Tutorial = 0,
-        Morning = 1,
-        Afternoon = 2,
-        Evening = 3
-    }
