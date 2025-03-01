92c148b90fd3b4db14d94659dea6011f5b1d0593
LevinStreet <swboulet@gmail.com>
2025-02-28 22:16:29 -0500

    FMOD kitchen and bathroom sfx added

--- Pareidolia/Assets/Object Interaction Scripts/Bathroom Interactables/CurtainOpenClose.cs
+++ Pareidolia/Assets/Object Interaction Scripts/Bathroom Interactables/CurtainOpenClose.cs
@@ -1,20 +1,24 @@
 using UnityEngine;
+using FMODUnity;
 
 public class CurtainOpenClose : MonoBehaviour
 {
     [SerializeField] private GameObject _closedCurtain;
     [SerializeField] private GameObject _openCurtain;
+    [SerializeField] EventReference showerCurtainSfx;
 
     private void CloseCurtain()
     {
         _closedCurtain.SetActive(true);
         _openCurtain.SetActive(false);
+        AudioManager.instance.PlayOneShot(showerCurtainSfx, this.transform.position);
     }
 
     private void OpenCurtain()
     {
         _closedCurtain.SetActive(false);
         _openCurtain.SetActive(true);
+        AudioManager.instance.PlayOneShot(showerCurtainSfx, this.transform.position);
     }
 
     void OnEnable()
