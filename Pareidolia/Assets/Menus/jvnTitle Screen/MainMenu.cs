69cd53d96a7f62e1d691db5245d0adc055d87594
jvn <73175916+JevenDev@users.noreply.github.com>
2025-03-01 01:35:06 -0500

    readded full title screen
    - re-added the full scene from the previous branch

--- Pareidolia/Assets/Menus/jvnTitle Screen/MainMenu.cs
+++ Pareidolia/Assets/Menus/jvnTitle Screen/MainMenu.cs
@@ -8,22 +8,18 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         Debug.Log("Play Game");
-        SceneManager.LoadScene("SampleScene");
     }
 
     public void QuitGame()
     {
         Debug.Log("Quit Game");
-        Application.Quit();
     }
     public void Options()
     {
         Debug.Log("Options");
-        // SceneManager.LoadScene("Options");
     }
     public void Credits()
     {
         Debug.Log("Credits");
-        // SceneManager.LoadScene("Credits");
     }
 }
