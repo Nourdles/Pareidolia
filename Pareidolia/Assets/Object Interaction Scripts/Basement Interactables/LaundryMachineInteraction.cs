a62f528b060513a30f0db2229000493d2e0c6fe0
villanueva-j <113061736+villanueva-j@users.noreply.github.com>
2025-03-05 18:22:11 -0500

    Implement LaundryMachineInteraction

--- Pareidolia/Assets/Object Interaction Scripts/Basement Interactables/LaundryMachineInteraction.cs
+++ Pareidolia/Assets/Object Interaction Scripts/Basement Interactables/LaundryMachineInteraction.cs
@@ -1,16 +1,41 @@
+using System;
 using UnityEngine;
 
-public class LaundryMachineInteraction : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+/// <summary>
+/// Logic for interaction with the washing machine BODY
+/// </summary>
+public class LaundryMachineInteraction : ObjectInteraction
+{
+    private bool _doorOpen = false;
+    public static event Action ClothesPutInWasherEvent;
+    public override void interact(GameObject objectInHand)
     {
-        
+        // if door not open, open it
+        // else if door open, proceed with below
+        if (objectInHand != null)
+        {
+            HandheldObjectInteraction objectInteraction = objectInHand.GetComponent<HandheldObjectInteraction>();
+            Handhelds handheld_id = objectInteraction.getHandheld();
+            if (handheld_id == Handhelds.LaundryBin)
+            {
+                if (((LaundryBinInteraction)objectInteraction).GetIsFull())
+                {
+                    // if yes: put laundry in machine + sfx
+                    ClothesPutInWasherEvent?.Invoke();
+                    
+                } else
+                {
+                    InvokeDialoguePromptEvent("I haven't picked up all my dirty clothes yet    " + 
+                        ((LaundryBinInteraction)objectInteraction).GetNumMissing());
+                }
+            } else
+            {
+                InvokeDialoguePromptEvent("I can't put this in the washing machine!");
+            }
+        } else
+        {
+            InvokeDialoguePromptEvent("I need to wash my dirty clothes");
+        }
     }
 }
