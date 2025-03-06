557da62d21082132cd7e82092b52e27ce11ff16f
villanueva-j <113061736+villanueva-j@users.noreply.github.com>
2025-03-05 22:08:18 -0500

    Update LaundryMachineInteraction.cs

--- Pareidolia/Assets/Object Interaction Scripts/Basement Interactables/LaundryMachineInteraction.cs
+++ Pareidolia/Assets/Object Interaction Scripts/Basement Interactables/LaundryMachineInteraction.cs
@@ -8,11 +8,11 @@ using UnityEngine;
 public class LaundryMachineInteraction : ObjectInteraction
 {
     private bool _doorOpen = false;
-    public static event Action ClothesPutInWasherEvent;
+    private bool _soapAdded = false;
+    private bool _clothesAdded = false;
+    public static event Action DoLaundryEvent;
     public override void interact(GameObject objectInHand)
     {
-        // if door not open, open it
-        // else if door open, proceed with below
         if (objectInHand != null)
         {
             HandheldObjectInteraction objectInteraction = objectInHand.GetComponent<HandheldObjectInteraction>();
@@ -22,12 +22,42 @@ public class LaundryMachineInteraction : ObjectInteraction
                 if (((LaundryBinInteraction)objectInteraction).GetIsFull())
                 {
                     // if yes: put laundry in machine + sfx
-                    ClothesPutInWasherEvent?.Invoke();
-                    
+                    _clothesAdded = true;
+                    if (!_soapAdded)
+                    {
+                        InvokeDialoguePromptEvent("Now I just need to add detergent");
+                    } else
+                    {
+                        if (_doorOpen)
+                        {
+                            // close door
+                        }
+                        StartLoad();
+                    } 
                 } else
                 {
                     InvokeDialoguePromptEvent("I haven't picked up all my dirty clothes yet    " + 
                         ((LaundryBinInteraction)objectInteraction).GetNumMissing());
+                }
+            } else if (handheld_id == Handhelds.Detergent)
+            {
+                if (_soapAdded)
+                {
+                    InvokeDialoguePromptEvent("I already added soap");
+                } else
+                {
+                    _soapAdded = true;
+                    if (_clothesAdded)
+                    {
+                        if (_doorOpen)
+                        {
+                            // close door
+                        }
+                        StartLoad();
+                    } else
+                    {
+                        InvokeDialoguePromptEvent("Now I just need to put my dirty clothes in");
+                    }
                 }
             } else
             {
@@ -37,5 +67,12 @@ public class LaundryMachineInteraction : ObjectInteraction
         {
             InvokeDialoguePromptEvent("I need to wash my dirty clothes");
         }
+    }
+
+    private void StartLoad()
+    {
+        // play washing machine sound
+        DoLaundryEvent?.Invoke();
+        SetUninteractable();
     }
 }
