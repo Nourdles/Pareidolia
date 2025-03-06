f7f00b112caa46ca7ad8be22d88c63cb34ee1f43
Nour Gatti <gattinour9@gmail.com>
2025-03-05 19:41:28 -0500

    changed house layout, added lighting effects, scripted events

--- Pareidolia/Assets/Object Interaction Scripts/Kitchen Interactables/FridgeDoorInteraction.cs
+++ Pareidolia/Assets/Object Interaction Scripts/Kitchen Interactables/FridgeDoorInteraction.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
 using FMODUnity;
+using System;
 
 public class FridgeDoorInteraction : ObjectInteraction
 {
     //public EventReference doorOpenSound;
     //public EventReference doorCloseSound;
     private bool doorOpen = false;
+    private bool hasOpenedOnce = false;
+    private bool hasClosedOnce = false;
     [SerializeField] Animator doorAnimator;
     [SerializeField] EventReference fridgeCloseSound;
     [SerializeField] EventReference fridgeOpenSound;
+    public static event Action OnFirstFridgeOpen;
+    public static event Action OnFirstFridgeClose;
 
 
     protected override void Start()
@@ -28,12 +33,24 @@ public class FridgeDoorInteraction : ObjectInteraction
             doorAnimator.Play("CloseFridgeDoor");
             Debug.Log("Door Closing");
             AudioManager.instance.PlayOneShot(fridgeCloseSound, this.transform.position);
+
+            if (!hasClosedOnce)
+            {
+                hasClosedOnce = true;
+                OnFirstFridgeClose?.Invoke();  // notify listeners
+            }
         }
         else
         {
             doorAnimator.Play("OpenFridgeDoor");
             Debug.Log("Door Opening");
             AudioManager.instance.PlayOneShot(fridgeOpenSound, this.transform.position);
+
+            if (!hasOpenedOnce)
+            {
+                hasOpenedOnce = true;
+                OnFirstFridgeOpen?.Invoke();  // notify listeners
+            }
         }
         doorOpen = !doorOpen;
     }
