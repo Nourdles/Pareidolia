4f5fcd717f97ab05537b986da0f2cb22e1c87560
villanueva-j <113061736+villanueva-j@users.noreply.github.com>
2025-03-07 01:23:12 -0500

    Update LoadScene.cs
    Added fxn to load game demo end screen

--- Pareidolia/Assets/Backend/LoadScene.cs
+++ Pareidolia/Assets/Backend/LoadScene.cs
@@ -22,6 +22,10 @@ public class LoadScene : MonoBehaviour
         SceneManager.LoadSceneAsync("MorningLevel");
     }
 
+    public static void LoadEndOfDemoScene()
+    {
+        SceneManager.LoadSceneAsync("GameDemoEnd");
+    }
 
 
 }
