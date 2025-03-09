64cf2bfadbc7ccab360b809996ae6fbbedbb3280
jvn <73175916+JevenDev@users.noreply.github.com>
2025-03-09 03:18:41 -0400

    Spritesheet update + interaction test
    - updated the spritesheet by consolidating all controls into one universal icon sheet (for controller)
    - added mockup for interaction prompt with UI icons

--- Pareidolia/Assets/Canvas UI/SpriteSheetManager.cs
+++ Pareidolia/Assets/Canvas UI/SpriteSheetManager.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class SpriteSheetManager : MonoBehaviour
 {
-    public Sprite[] PlaystationUISprites;
-    public Sprite[] XboxUISprites;
     public Sprite[] PcUISprites;
 
-    // public Image 
     public int ID;
 
     void Start()
     {
-        PlaystationUISprites = Resources.LoadAll<Sprite>("UISprites/PlaystationUI");
-        XboxUISprites = Resources.LoadAll<Sprite>("UISprites/XboxUI");
-        PcUISprites = Resources.LoadAll<Sprite>("UISprites/PCUI");
+        PcUISprites = Resources.LoadAll<Sprite>("UISprites");
     }
 
     // Update is called once per frame
