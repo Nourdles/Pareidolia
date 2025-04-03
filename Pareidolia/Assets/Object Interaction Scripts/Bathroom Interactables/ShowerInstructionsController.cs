1ef02f9590f98f70b2927c706d4b68afb6042cd3
villanueva-j <113061736+villanueva-j@users.noreply.github.com>
2025-04-03 01:15:58 -0400

    Fixes issue with shower task instructions not appearing consistently in the shower

---
+++ Pareidolia/Assets/Object Interaction Scripts/Bathroom Interactables/ShowerInstructionsController.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShowerInstructionsController : MonoBehaviour
+{
+    private InputAction interactKey;
+    private string inputMasking;
+    public static event Action<string> ShowerInstructionsEvent;
+
+    void Start()
+    {
+        interactKey = InputSystem.actions.FindAction("Interact");
+    }
+
+    private void TriggerShowerInstructions()
+    {
+        ShowerInstructionsEvent?.Invoke("Hold <sprite=\"UISprites\" name=\"" + 
+                interactKey.GetBindingDisplayString(InputBinding.MaskByGroup(inputMasking)) + "\"> to shower");
+    }
+
+    private void UpdateControllerScheme(bool usingKBM)
+    {
+        if (usingKBM)
+        {
+            inputMasking = "Keyboard&Mouse";
+        } else
+        {
+            inputMasking = "Gamepad";
+        }
+    }
+
+    void OnEnable()
+    {
+        InputDeviceChecker.UsingKBMEvent += UpdateControllerScheme;
+        ShowerInteraction.GetIntoTubEvent += TriggerShowerInstructions;
+    }
+
+    void OnDisable()
+    {
+        InputDeviceChecker.UsingKBMEvent -= UpdateControllerScheme;
+        ShowerInteraction.GetIntoTubEvent += TriggerShowerInstructions;
+    }
+}
