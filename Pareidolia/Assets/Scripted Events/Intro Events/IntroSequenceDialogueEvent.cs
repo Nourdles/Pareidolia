61c3907c2e5c247e47e867b1b9c9c98e7373db79
Nour Gatti <gattinour9@gmail.com>
2025-04-11 17:08:32 -0400

    damage effect on stain in intro

--- Pareidolia/Assets/Scripted Events/Intro Events/IntroSequenceDialogueEvent.cs
+++ Pareidolia/Assets/Scripted Events/Intro Events/IntroSequenceDialogueEvent.cs
@@ -1,12 +1,34 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class IntroSequenceDialogueEvent : MonoBehaviour
 {
-
     [SerializeField] private LayerMask playerLayer;
     [SerializeField] private int dialogueTriggerNum;
     public static event Action<string> IntroDialogueEvent;
+    [SerializeField] private Volume postProcessingVolume;
+
+    private FilmGrain filmGrain;
+    private DepthOfField depthOfField;
+    private Coroutine distortionRoutine;
+
+    private void Start()
+    {
+        if (postProcessingVolume != null)
+        {
+            postProcessingVolume.profile.TryGet(out filmGrain);
+            postProcessingVolume.profile.TryGet(out depthOfField);
+
+            if (filmGrain != null)
+                filmGrain.intensity.overrideState = true;
+
+            if (depthOfField != null)
+                depthOfField.focusDistance.overrideState = true;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,8 +37,16 @@ public class IntroSequenceDialogueEvent : MonoBehaviour
             if (dialogueTriggerNum == 1)
             {
                 IntroDialogueEvent?.Invoke("What is that?");
-                // disable trigger so it cant be triggered again
-                gameObject.SetActive(false);
+
+                // damage effect to teach player
+                if (filmGrain != null && depthOfField != null)
+                {
+                    if (distortionRoutine != null)
+                        StopCoroutine(distortionRoutine);
+
+                    distortionRoutine = StartCoroutine(AnimateDistortion());
+                }
+                // do not disable immediately – will be handled at end of coroutine
             }
             else if (dialogueTriggerNum == 2)
             {
@@ -32,5 +62,48 @@ public class IntroSequenceDialogueEvent : MonoBehaviour
         return (playerLayer.value & (1 << obj.layer)) != 0;
     }
 
+    private IEnumerator AnimateDistortion()
+    {
+        float grainMin = 0.1f;
+        float grainMax = 1f;
+        float dofMin = 0.1f;
+        float dofMax = 10f;
+        float fadeDuration = 0.2f;
+        float holdDuration = 0.8f;
 
+        // fade in
+        float timer = 0f;
+        while (timer < fadeDuration)
+        {
+            float t = timer / fadeDuration;
+            filmGrain.intensity.value = Mathf.Lerp(grainMin, grainMax, t);
+            depthOfField.focusDistance.value = Mathf.Lerp(dofMax, dofMin, t);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        filmGrain.intensity.value = grainMax;
+        depthOfField.focusDistance.value = dofMin;
+
+        // hold
+        yield return new WaitForSeconds(holdDuration);
+
+        // fade out
+        timer = 0f;
+        while (timer < fadeDuration)
+        {
+            float t = timer / fadeDuration;
+            filmGrain.intensity.value = Mathf.Lerp(grainMax, grainMin, t);
+            depthOfField.focusDistance.value = Mathf.Lerp(dofMin, dofMax, t);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        filmGrain.intensity.value = grainMin;
+        depthOfField.focusDistance.value = dofMax;
+        distortionRoutine = null;
+
+        // NOW disable the object
+        gameObject.SetActive(false);
+    }
 }
