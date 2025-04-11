af976a57ff111d93499e3ac4370a8676a841552b
Sebastian Lievano Arzayus <lievseb@gmail.com>
2025-04-10 23:47:59 -0700

    New Death Scene

--- Pareidolia/Assets/DeathManager.cs
+++ Pareidolia/Assets/DeathManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Threading.Tasks;
 using Unity.VisualScripting;
+using static UnityEngine.UI.Image;
 
 public class DeathManager : MonoBehaviour
 {
@@ -61,7 +62,7 @@ public class DeathManager : MonoBehaviour
         blackPanel.color = new Color(0, 0, 0, 0);
     }
 
-    private IEnumerator SpawnDeathStain(RaycastHit hit)
+    private void SpawnDeathStain(RaycastHit hit)
     {
         Vector3 pos = hit.point;
         Vector3 normal = hit.normal;
@@ -78,22 +79,51 @@ public class DeathManager : MonoBehaviour
             sr.sprite = bodySprite;
             sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 1f);
         }
-        yield break;
 
     }
 
 
-    private IEnumerator SpawnStainAtPlayerPosition()
+    private void SpawnStainAtPlayerPosition()
     {
         RaycastHit floorHit;
-        if (Physics.Raycast(Player.transform.position, Vector3.down, out floorHit, 30f))
+
+
+        RaycastHit[] hits = Physics.RaycastAll(Player.transform.position, Vector3.down, 35f);
+        System.Array.Sort(hits, (a, b) => a.distance.CompareTo(b.distance));
+        foreach (RaycastHit hit in hits)
         {
-            if(floorHit.collider.CompareTag("Wood"))
+            if (hit.collider == null) continue;
+            if (hit.collider.CompareTag("Wood"))
             {
-                StartCoroutine(SpawnDeathStain(floorHit));
+                Debug.Log("Spawning stain");
+                SpawnDeathStain(hit);
+                break;
             }
         }
-        yield break;
+
+
+    }
+
+    private IEnumerator MoveToPositionAndRotation(Transform transform, Vector3 position, Quaternion rotation, float duration)
+    {
+        Vector3 startPos = transform.position;
+        Quaternion startRot = transform.rotation;
+        Vector3 endPos = position;
+        Quaternion endRot = rotation;
+
+        float time = 0f;
+        while (time < duration)
+        {
+            float t = time / duration;
+            transform.position = Vector3.Lerp(startPos, endPos, t);
+            transform.rotation = Quaternion.Slerp(startRot, endRot, t);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        // Final snap to make sure it's exact
+        transform.position = endPos;
+        transform.rotation = endRot;
     }
 
     public IEnumerator ProcessDeath(GameObject murderingStain, Vector3 normal)
@@ -101,10 +131,9 @@ public class DeathManager : MonoBehaviour
         DeathSceneEvent?.Invoke();
         DeathEvent?.Invoke("I feel dizzy. What's going on??");
         SanityTracker.damageEnabled = false;
-        blackScreen();
 
 
-        StartCoroutine(SpawnStainAtPlayerPosition());
+        SpawnStainAtPlayerPosition();
         cc.enabled = false;
         cameraMovement.enabled = false;
 
@@ -113,20 +142,31 @@ public class DeathManager : MonoBehaviour
         Quaternion originalLocalRotation = cameraPosition.transform.localRotation;
         Transform originalParent = cameraPosition.transform.parent;
 
-        //Detach camera from player and move it to stain
-        cameraPosition.transform.parent = null;
-        cameraPosition.transform.position = murderingStain.transform.position;
-        cameraPosition.transform.rotation = Quaternion.LookRotation(normal);
-
-        yield return new WaitForSeconds(1.2f);
+        //Retrieve position on ceiling
 
 
-        //Player.transform.position = murderingStain.transform.position;
+        RaycastHit[] hits = Physics.RaycastAll(Player.transform.position, Vector3.up, 30f);
 
-        disableBlackScreen();
+        RaycastHit hit = hits[0];
+        bool foundCeiling = false;
+        foreach (RaycastHit h in hits)
+        {
+            if (h.collider.CompareTag("Ceiling"))
+            {
+                hit = h;
+                foundCeiling = true;
+                break;
+            }
+        }
+        //Only if we can
+        if (foundCeiling)
+        {
+            Vector3 pos = hit.point + new Vector3(0, -1f, 0);
+            //Detach camera from player and move it to stain
+            StartCoroutine(MoveToPositionAndRotation(cameraPosition.transform, pos, Quaternion.LookRotation(hit.normal), 5f));
+            yield return new WaitForSeconds(5f);
 
-        yield return new WaitForSeconds(5f);
-
+        }
 
         blackScreen();
 
